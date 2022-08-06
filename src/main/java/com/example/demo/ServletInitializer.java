ae85af97f67c760f6cf07ec61e8812be6a66d701
renangton <puccho0427@gmail.com>
2022-08-06 22:06:38 +0900

    Checkstyleが通るように修正

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder applicatiFFFFFFFFFFFFFon) {
-    return applicatiFFFFFFFFFFFFFon.sources(BananaCrudRestfulapiApplication.class);
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(BananaCrudRestfulapiApplication.class);
   }
 }
